182db01c060d29abc8a08fec195542c2074f2541
Darshan_Moradiya <darshancodes99@gmail.com>
2024-07-02 10:18:52 +0530

    lastOccurance

--- src/LastOccurance.java
+++ src/LastOccurance.java
@@ -1,6 +1,19 @@
 public class LastOccurance {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 5};
+        System.out.println(findLastOccurrence(arr, 5, arr.length -1));
+    }
+    private static int findLastOccurrence(int[] array, int element, int index) {
+
+        if (index < 0) {
+            return -1;
+        }
+
+        if (array[index] == element) {
+            return index;
+        }
+
+        return findLastOccurrence(array, element, index - 1);
     }
 
 }
