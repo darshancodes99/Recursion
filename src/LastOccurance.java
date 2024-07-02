105d9705c88d2760c0302fa19321ab64f0015bdc
Darshan_Moradiya <darshancodes99@gmail.com>
2024-07-02 10:15:09 +0530

    changes in last occurancre and sum of array

--- src/LastOccurance.java
+++ src/LastOccurance.java
@@ -1,6 +1,17 @@
 public class LastOccurance {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 5};
+        System.out.println(lastOccurence(arr,arr.length - 1, 5));
     }
 
+    public static int lastOccurence(int[] arr, int i, int key) {
+        if (i < 0) {
+            return -1;
+        }
+        if (arr[i] == key) {
+            return i;
+        }
+
+        return lastOccurence(arr, i - 1, key);
+    }
 }
--- src/Sum_of_array.java
+++ src/Sum_of_array.java
@@ -1,14 +1,13 @@
 public class Sum_of_array {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        int n = arr.length;
-        System.out.println(sum(arr, n));
+        System.out.println(sum(arr, 0));
     }
 
     public static int sum(int[] arr, int n) {
-        if (n <= 0) {
+        if (n == arr.length) {
             return 0;
         }
-        return (sum(arr, n - 1) + arr[n - 1]);
+        return arr[n] + sum(arr, n + 1);
     }
 }
